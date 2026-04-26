be8c5636224876a64f0143ffa3795895d2f72dc3
in6cure <akashmeena1072@gmail.com>
2026-04-27 03:11:34 +0530

    Update main.py

--- main.py
+++ main.py
@@ -42,7 +42,7 @@ if st.button("Solve Step-by-Step"):
             """
             
             response = client.models.generate_content(
-                model='gemini-1.5-flash',
+                model='gemini-1.5-flash-latest',
                 contents=prompt,
             )
             
